eee8b47a24984c72ad39728ec9f530ca44417dae
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-15 22:25:49 +0800

    second deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
 import svgLoader from "vite-svg-loader";
 import vue from "@vitejs/plugin-vue";
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Vue_Tool_Website/",
-  plugins: [
-    createSvgIconsPlugin({
-      // 指定需要缓存的svg图标文件目录
-      iconDirs: [path.resolve(process.cwd(), "public/SVG")], // 指定symbolId格式
-      symbolId: "icon-[dir]-[name]",
-    }),
-    vue(),
-  ],
+  plugins: [vue()],
 });
