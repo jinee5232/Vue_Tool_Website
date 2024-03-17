c953c320629394f67a086c35c6c619e4ae6bf594
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-17 14:59:16 +0800

    json url

--- src/pages/DGraphic.vue
+++ src/pages/DGraphic.vue
@@ -130,7 +130,7 @@ export default {
   },
   methods: {
     fetchWebData() {
-      fetch("public/webdata.json")
+      fetch("/public/webdata.json")
         .then((response) => response.json())
         .then((data) => {
           this.webData = data;
