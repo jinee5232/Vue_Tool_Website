a20d0603ff4c4dc80e57fef8f417cdce6f4f2adf
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-17 14:40:36 +0800

    create /Vue_Tool_Website/

--- src/routes.js
+++ src/routes.js
@@ -13,22 +13,22 @@ const routes = [
     component: HomePage,
   },
   {
-    path: "/Vue_Tool_Website//DInspirationPage",
+    path: "/Vue_Tool_Website/DInspirationPage",
     name: "DInspiration",
     component: DInspiration,
   },
   {
-    path: "/Vue_Tool_Website//DGraphicPage",
+    path: "/Vue_Tool_Website/DGraphicPage",
     name: "DGraphic",
     component: DGraphic,
   },
   {
-    path: "/Vue_Tool_Website//DCssPage",
+    path: "/Vue_Tool_Website/DCssPage",
     name: "DCss",
     component: DCss,
   },
   {
-    path: "/Vue_Tool_Website//OtherToolPage",
+    path: "/Vue_Tool_Website/OtherToolPage",
     name: "OtherTool",
     component: OtherTool,
   },
